c6335d0843cc8aed3ed6f5ec7db5f179890ed645
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-02-05 12:02:49 +0300

    feat: add auth pkg

--- pkg/auth/jwt.go
+++ pkg/auth/jwt.go
@@ -1,1 +1,31 @@
 package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func SignJWT(subject string, secret []byte, ttl time.Duration) (string, error) {
+	iat := time.Now()
+	exp := iat.Add(ttl)
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Subject:   subject,
+		IssuedAt:  jwt.NewNumericDate(iat),
+		ExpiresAt: jwt.NewNumericDate(exp),
+	}).SignedString(secret)
+}
+
+func VerifyJWT(tokenString string, secret []byte) (string, error) {
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+		return secret, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if !token.Valid {
+		return "", jwt.ErrSignatureInvalid
+	}
+	return claims.Subject, nil
+}
--- pkg/auth/password.go
+++ pkg/auth/password.go
@@ -1,1 +1,14 @@
 package auth
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func ComparePassword(hash []byte, password string) bool {
+	err := bcrypt.CompareHashAndPassword(hash, []byte(password))
+	return err == nil
+}
